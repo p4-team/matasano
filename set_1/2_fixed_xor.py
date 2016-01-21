644120fe91c3e4fe609a4a28513639d05725e2f2
Pharisaeus <spodgorski@gmail.com>
2016-01-21 21:08:06 +0100

    More verbose output for 2

--- set_1/2_fixed_xor.py
+++ set_1/2_fixed_xor.py
@@ -1,11 +1,13 @@
 def xor(in1, in2):
-    return "".join([format(ord(x[0]) ^ ord(x[1]), "x") for x in zip(in1, in2)])
+    return "".join([chr(ord(x[0]) ^ ord(x[1])) for x in zip(in1, in2)])
 
 
 def main():
     in1 = "1c0111001f010100061a024b53535009181c".decode("hex")
     in2 = "686974207468652062756c6c277320657965".decode("hex")
-    print(xor(in1, in2))
+    result = xor(in1, in2)
+    print(result)
+    print(result.encode("hex"))
 
 
 if __name__ == '__main__':
