54a9d84af7b97a9fcd819aa2c9b249560053b629
msm2e4d534d <msm2e4d534d@gmail.com>
2016-01-21 21:11:14 +0100

    Solve challenge 3

---
+++ set_1/3_single_byte_xor.py
@@ -0,0 +1,27 @@
+import langdetect
+
+def is_plaintext(data):
+    return all(32 <= ord(c) <= 126 for c in data)
+
+def xor(a, b):
+    return ''.join(chr(ord(ac) ^ ord(bc)) for ac, bc in zip(a, b))
+
+def decrypt_bruteforce(data):
+    l = len(data)
+    possible = []
+    for b in range(256):
+        next = xor(data, chr(b) * l)
+        if is_plaintext(next):
+            langs = [lng for lng in langdetect.detect_langs(next) if lng.lang == 'en']
+            if langs:
+                possible.append([next, max(lng.prob for lng in langs)])
+    solutions = sorted(possible, key=lambda x: x[1], reverse=True)
+    for sln in solutions[:5]:
+        print sln[1], sln[0]
+
+def main():
+    test_data = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    print decrypt_bruteforce(test_data.decode('hex')) 
+
+if __name__ == '__main__':
+    main()
