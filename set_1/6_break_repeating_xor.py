6567986cceee9cfb9bd7778190838a445fe58248
msm2e4d534d <msm2e4d534d@gmail.com>
2016-01-21 21:32:59 +0100

    Fix challenge 3

--- set_1/6_break_repeating_xor.py
+++ set_1/6_break_repeating_xor.py
@@ -3,6 +3,27 @@ import langdetect
 def is_plaintext(data):
     return all(32 <= ord(c) <= 126 for c in data)
 
+def hamming(x, y):
+    count, z = 0, int(x.encode('hex'),16) ^ int(y.encode('hex'),16)
+    while z:
+        if z & 1:
+            count += 1
+        z >>= 1
+    return count
+
+"""
+compute index of coincidence of data
+"""
+def ioc(data, shift):
+    r = 0
+    for i in range(len(data) - shift):
+        if data[i] == data[i + shift]:
+            r += 1
+    return r / float(len(data) - shift)
+
+print hamming('this is a test', 'wokka wokka!!!')
+
+
 def xor(a, b):
     return ''.join(chr(ord(ac) ^ ord(bc)) for ac, bc in zip(a, b))
 
