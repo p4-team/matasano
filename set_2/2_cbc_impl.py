6eea3453204d881b78f64c82173f7519adbc04d8
Pharisaeus <spodgorski@gmail.com>
2016-02-08 00:45:13 +0100

    fixed implementation (wrong xor arg)

--- set_2/2_cbc_impl.py
+++ set_2/2_cbc_impl.py
@@ -8,32 +8,29 @@ def xor(in1, in2):
     return "".join([chr(ord(x) ^ ord(y)) for x, y in zip(in1, in2)])
 
 
-def decrypt_ecb(line):
-    key = "YELLOW SUBMARINE"
+def decrypt_ecb(line, key):
     cipher = AES.new(key, AES.MODE_ECB)
     return cipher.decrypt(line)
-
-
-def encrypt(line):
-    key = "YELLOW SUBMARINE"
-    cipher = AES.new(key, AES.MODE_ECB)
-    return cipher.encrypt(line)
 
 
 def chunk(input_data, size):
     return [input_data[i * size:(i + 1) * size] for i in range(len(input_data) / size)]
 
 
-def decrypt_cbc(input_data, IV):
-    result_blocks = [IV]
+def decrypt_cbc(input_data, key, IV):
+    result_blocks = []
+    previous_block = IV
     for block in chunk(input_data, len(IV)):
-        decrypted_ecb = decrypt_ecb(block)
-        xored = xor(decrypted_ecb, result_blocks[-1])
+        decrypted_ecb = decrypt_ecb(block, key)
+        xored = xor(decrypted_ecb, previous_block)
+        previous_block = block
         result_blocks.append(xored)
     return "".join(result_blocks)
 
 
-with codecs.open("2.txt", "r") as input_file:
-    text = "".join(input_file.read().splitlines())
-    text = base64.b64decode(text)
-    print(decrypt_cbc(text, "\x00" * 16))
+if __name__ == "__main__":
+    with codecs.open("2.txt", "r") as input_file:
+        text = "".join(input_file.read().splitlines())
+        text = base64.b64decode(text)
+        key = "YELLOW SUBMARINE"
+        print(decrypt_cbc(text, key, "\x00" * 16))
