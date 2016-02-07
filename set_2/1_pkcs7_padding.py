7666ec1f3985eb6b8c78e043bf930cd8c6faf6c4
msm2e4d534d <msm2e4d534d@gmail.com>
2016-02-08 00:22:41 +0100

    Add solution for challenge 2.1

--- set_2/1_pkcs7_padding.py
+++ set_2/1_pkcs7_padding.py
@@ -1,7 +1,12 @@
-def test():
-    # todo
-    pass
+def pkcs7pad(dat, n=16):
+    pad_len = n - (len(dat) % n)
+    if pad_len == 0:
+        pad_len = n
+    return dat + chr(pad_len) * pad_len
+    
 
+def test():
+    assert pkcs7pad("YELLOW SUBMARINE", 20) == "YELLOW SUBMARINE\x04\x04\x04\x04"
 
 if __name__ == '__main__':
     test()
