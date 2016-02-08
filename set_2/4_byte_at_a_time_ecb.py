88348febb2519da44f75fab936ed4ff285fc9c82
Pharisaeus <spodgorski@gmail.com>
2016-02-08 21:35:28 +0100

    finished ecb secret brute byte at a time

--- set_2/4_byte_at_a_time_ecb.py
+++ set_2/4_byte_at_a_time_ecb.py
@@ -1,6 +1,10 @@
 import base64
 import random
+import string
+
 from Crypto.Cipher import AES
+
+key = "".join([chr(random.randint(0, 255)) for i in range(16)])
 
 
 def decrypt_ecb(line, key):
@@ -14,8 +18,7 @@ def encrypt_ecb(line, key):
 
 
 def blackbox_ecb_encrypt(data):
-    key = "".join([chr(random.randint(0, 255)) for i in range(16)])
-    secret = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
+    secret = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
     decoded_secret = base64.b64decode(secret)
     input_data = data + decoded_secret
     padding = "\x00" * (16 - len(input_data) % 16)
@@ -38,9 +41,40 @@ def when_length_changed(init_data):
 def brute_block_len():
     first_change = when_length_changed("")
     second_change = when_length_changed("\x00" * first_change)
-    return first_change, second_change
+    return second_change
+
+
+def brute_secret(block_len):
+    secret_len = len(blackbox_ecb_encrypt(""))
+    result = []
+    bruting_block_number = secret_len / block_len - 1
+    for i in range(1, secret_len + 1):
+        data = "\x00" * (secret_len - i)
+        encrypted = blackbox_ecb_encrypt(data)
+        blocks = chunk(encrypted, block_len)
+        reference_block = blocks[bruting_block_number]
+        result.append(brute_character(data, result, bruting_block_number, reference_block))
+    return "".join(result)
+
+
+def brute_character(data, current_result, bruting_block_number, reference_block):
+    for character in string.printable:
+        brute_data = data + "".join(current_result) + character
+        encrypted = blackbox_ecb_encrypt(brute_data)
+        blocks = chunk(encrypted, block_len)
+        result_block = blocks[bruting_block_number]
+        if reference_block == result_block:
+            print("found " + str(i) + " " + character)
+            return character
+    return "?"  # padding
+
+
+def chunk(input_data, size):
+    return [input_data[i * size:(i + 1) * size] for i in range(len(input_data) / size)]
 
 
 if __name__ == "__main__":
-    filling_first_block, key_len = brute_block_len()
-    print(filling_first_block, key_len)
+    block_len = brute_block_len()
+    print(block_len)
+    recovered_secret = brute_secret(block_len)
+    print(recovered_secret)
